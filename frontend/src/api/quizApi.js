cf1499579e79c9e9e6cd0c907e7c46b2767dc847
정민규 <hsalsrb0321@naver.com>
2026-06-22 10:47:20 +0900

    ✨ feat : 퀴즈 풀기 화면 구현 - QuizCard, QuizAnswerCard, quizApi (#8)

---
+++ frontend/src/api/quizApi.js
@@ -0,0 +1,30 @@
+import axiosInstance from './axiosInstance'
+
+export const quizApi = {
+  /**
+   * 기사 기반 퀴즈 목록 조회 (정답 제외)
+   * @param {number|string} articleId
+   */
+  getQuizList(articleId) {
+    return axiosInstance.get(`/articles/${articleId}/quiz`)
+  },
+
+  /**
+   * 퀴즈 답변 제출 및 채점
+   * @param {number|string} quizId
+   * @param {Object} payload { userAns: string }
+   */
+  submitAnswer(quizId, payload) {
+    return axiosInstance.post(`/quiz/${quizId}/answer`, payload)
+  },
+
+  /**
+   * 퀴즈 결과 조회
+   * @param {number|string} quizId
+   */
+  getQuizResult(quizId) {
+    return axiosInstance.get(`/quiz/${quizId}/result`)
+  }
+}
+
+export default quizApi
